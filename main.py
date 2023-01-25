134e9b68d4581df8237607b2bbacd8424eac7352
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-24 21:52:46 -0500

    completes game and adds feature to export list of missed states

--- main.py
+++ main.py
@@ -14,18 +14,29 @@ screen.addshape(image)
 turtle.shape(image)
 screen.setup(width=800, height=600)
 
-answer_state = screen.textinput(
-    title="Guess the state", prompt="Enter a state").title()
-print(answer_state)
-if answer_state in states:
-    if answer_state not in states_guessed:
-        state_data = data[data.state == answer_state]
-        print("state guessed")
-        print(state_data.x)
-        states_guessed.append(answer_state)
+game = True
+while len(states_guessed) < 50 and game:
+    answer_state = screen.textinput(
+        title=f"{len(states_guessed)}/50 Correct", prompt="Guess a state!").title()
+    print(answer_state)
+    if answer_state == "Exit" or answer_state == "Quit":
+        game = False
+        break
+    if answer_state in states:
+        if answer_state not in states_guessed:
+            state_data = data[data.state == answer_state]
+            print("state guessed")
+            print(state_data.x)
+            states_guessed.append(answer_state)
+            new_state = State(
+                (int(state_data.x), int(state_data.y)), answer_state)
 
-        new_state = State(
-            (int(state_data.x), int(state_data.y)), answer_state)
+missing_states = []
+for state in states:
+    if state not in states_guessed:
+        missing_states.append(state)
 
+missed_states = pd.DataFrame(missing_states)
+missed_states.to_csv("missed_states.csv")
 
 turtle.mainloop()

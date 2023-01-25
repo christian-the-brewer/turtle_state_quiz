3d32e639d249a731fdd435d399c594b5fa6d0254
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-24 19:26:10 -0500

    plots correct answers to map

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import pandas as pd
 from state import State
 
 data = pd.read_csv("50_states.csv")
-states = data.state.to_list()
 
+states = data.state.to_list()
+states_guessed = []
 
 screen = turtle.Screen()
 screen.title("U.S.A. States Guessing Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+screen.setup(width=800, height=600)
 
 answer_state = screen.textinput(
     title="Guess the state", prompt="Enter a state").title()
 print(answer_state)
 if answer_state in states:
-    pass
+    if answer_state not in states_guessed:
+        state_data = data[data.state == answer_state]
+        print("state guessed")
+        print(state_data.x)
+        states_guessed.append(answer_state)
+
+        new_state = State(
+            (int(state_data.x), int(state_data.y)), answer_state)
 
 
 turtle.mainloop()
